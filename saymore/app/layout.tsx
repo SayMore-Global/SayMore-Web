23681863e40b3d64c64989716ce19475b41d8a0b
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2025-02-24 14:13:18 +0530

    fix: layout.tsx: Update favicon path

--- saymore/app/layout.tsx
+++ saymore/app/layout.tsx
@@ -13,7 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        {/*<link rel="icon" href="/favicon.png" />*/}
+        <link rel="icon" href="/SayMore-Web/favicon.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
